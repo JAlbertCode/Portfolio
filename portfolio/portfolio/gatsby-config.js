7af891d885dd50260c1055e40259a37f7fc7cece
Jonathan Albert <JonathanAlbert0115@gmail.com>
2020-11-03 14:11:12 -0500

    updated google analytics tracking id

--- portfolio/portfolio/gatsby-config.js
+++ portfolio/portfolio/gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "G-XNTFMT7G84",
+        trackingId: "G-G6MWPTS9G2",
         head: true
       }
     },
